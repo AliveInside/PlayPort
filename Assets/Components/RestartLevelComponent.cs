897e6531ffbb75b619f625482f53850918621310
AliveInside <the_banana@mail.ru>
2024-04-14 17:31:20 +0200

    Add homework #6

---
+++ Assets/Components/RestartLevelComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace PlayPort.Components
+{
+    public class RestartLevelComponent : MonoBehaviour
+    {
+        private Hero _hero;
+        private void Start()
+        {
+            _hero = FindObjectOfType<Hero>();
+        }
+
+        public void Restart()
+        {
+            _hero.AddCoins(0);
+            var scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace PlayPort
+{
+    public class FollowTarget : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _smoothing;
+
+        private void LateUpdate()
+        {
+            var destination = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime * _smoothing);
+        }
+    }
+}
