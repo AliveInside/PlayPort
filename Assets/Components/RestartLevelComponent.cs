9dfeedfde8865ff6652fde0776e39cdd3e8951ac
AliveInside <the_banana@mail.ru>
2024-04-14 17:29:34 +0200

    Add homework #4

---
+++ Assets/Components/RestartLevelComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PlayPort.Components
+{
+    public class RestartLevelComponent : MonoBehaviour
+    {
+        public void Restart()
+        {
+            var scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
