ef21e17550e8dee6ec5cca91bf7cda933f674d3a
AliveInside <the_banana@mail.ru>
2024-04-14 17:34:06 +0200

    Add coursework #1

---
+++ Assets/Components/HPChangeComponent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayPort.Components
+{
+    public class HPChangeComponent : MonoBehaviour
+    {
+        [SerializeField] private int _amount;
+        [SerializeField] private bool _isDamage;
+
+        [SerializeField] private TypeOfChange _type;
+        public void HPChange(GameObject target)
+        {
+            var healthComponent = target.GetComponent<HealthComponent>();
+            var amount = _amount;
+
+            if (_type == TypeOfChange.Damage)
+            {
+                amount *= -1;
+            }
+
+            healthComponent?.HealthChange(amount);
+        }
+
+        public enum TypeOfChange
+        {
+            Heal,
+            Damage
+        }
+    }
+}
