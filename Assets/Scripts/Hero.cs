2361a12502ef43f5062e62fe74506581992ab0b5
AliveInside <the_banana@mail.ru>
2024-04-14 17:27:19 +0200

    Add homework #2

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+namespace PlayPort
+{
+    public class Hero : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private float _jumpForce;
+        //private float _direction;
+        private Vector2 _direction;
+        
+        public void SetDirection(Vector2 direction)
+        {
+            _direction = direction;
+        }
+
+        
+        private void Update()
+        {
+            if (_direction[0] != 0)
+            {
+                var delta = _direction[0] * _speed * Time.deltaTime;
+                var newXPosition = transform.position.x + delta;
+                transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+            }
+            
+            if (_direction[1] != 0)
+            {
+                var delta = _direction[1] * _speed * Time.deltaTime;
+                var newYPosition = transform.position.y + delta;
+                transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+            }
+        }
+
+        public void Speech()
+        {
+            Debug.Log("Speeeeech");
+        }
+
+    }
+}
